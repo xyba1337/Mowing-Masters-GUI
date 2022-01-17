2e89decdc7921c87893d9299e1493e4ea957f46a
xyba1337 <62798507+xyba1337@users.noreply.github.com>
2022-01-17 19:12:53 +0100

    Update MowingMastersGUI.lua

--- MowingMastersGUI.lua
+++ MowingMastersGUI.lua
@@ -208,3 +208,11 @@ function getAllGamepasses()
         Duration = 5;
     })
 end
+
+--anti afk by Sigmarillion
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
